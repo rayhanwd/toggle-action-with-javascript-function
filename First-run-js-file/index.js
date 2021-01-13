451c7bda06578afa641f12c139747db5cfe8eb42
Kazi Rayhan <rayhanbd4400@gmail.com>
2021-01-13 04:57:06 -0500

    toggle action with javascript function

---
+++ First-run-js-file/index.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    var a = document.getElementById("text");
+    if (a.style.display === "none"){
+        a.style.display ="block";
+    }
+    else{
+        a.style.display ="none";
+    }
+}
